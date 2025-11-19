e403b4ebb93131006e2666b471c37a3fa92dbd96
Elisabeth Hvalgren <ehv@kth.se>
2025-11-19 14:23:37 +0100

    Fire

--- interaction-3.js
+++ interaction-3.js
@@ -5,8 +5,8 @@ let dspNode = null;
 let dspNodeParams = null;
 let jsonParams = null;
 
-// Namnet måste matcha engine.wasm
-const dspName = "engine";
+
+const dspName = "fire";
 const instance = new FaustWasm2ScriptProcessor(dspName);
 
 // output to window or npm package module
@@ -19,7 +19,7 @@ if (typeof module === "undefined") {
 }
 
 // Skapa DSP från engine.wasm
-engine.createDSP(audioContext, 1024)
+fire.createDSP(audioContext, 1024)
     .then(node => {
         dspNode = node;
         dspNode.connect(audioContext.destination);
@@ -28,14 +28,7 @@ engine.createDSP(audioContext, 1024)
         jsonParams = JSON.parse(jsonString)["ui"][0]["items"];
         dspNodeParams = jsonParams;
 
-        // (valfritt) sätt lite default-värden
-        dspNode.setParamValue("/engine/brushLevel", 0.9);
-        dspNode.setParamValue("/engine/rotorLevel", 0.6);
-        dspNode.setParamValue("/engine/statorLevel", 0.7);
-        dspNode.setParamValue("/engine/tubeRes", 0.2);
-        dspNode.setParamValue("/engine/runtime", 5.0);
-        dspNode.setParamValue("/engine/gate", 0);   // starta med motor av
-        dspNode.setParamValue("/engine/volume", 0.3);
+    
     });
 
 
@@ -116,11 +109,11 @@ function playAudio(pressure) {
     const p = Math.max(0, Math.min(1, pressure)); // clamp 0..1
 
     // Slå på motorn om vi har lite tryck
-    dspNode.setParamValue("/engine/gate", p > 0.05 ? 1 : 0);
+    dspNode.setParamValue("/fire/gate", p > 0.05 ? 1 : 0);
 
     // Koppla pressure till maxSpeed och volume
-    dspNode.setParamValue("/engine/maxSpeed", p);
-    dspNode.setParamValue("/engine/volume", 0.2 + 0.8 * p);
+    dspNode.setParamValue("/fire/maxSpeed", p);
+    dspNode.setParamValue("/fire/volume", 0.2 + 0.8 * p);
 }
 
 // Tilt-funktion (roll)
